01bf1630e9311299f86904deba1cdaeb7d0afd29
FRANCISCO OMAR <115811507+pacoomar@users.noreply.github.com>
2023-03-21 17:53:49 +0100

    UD4 Ejemplo Apuntes Funciones

---
+++ funciones.java
@@ -0,0 +1,18 @@
+//import java.util.Scanner;
+
+public class funciones {
+    private static int suma(int numero1, int numero2) {
+        int suma;
+        suma = numero1 + numero2;
+        return suma;
+
+    }
+
+    public static void main(String args[]) {
+        int suma1, suma2, numero1, numero2;
+        suma1 = suma(5, 7);
+        suma2 = suma(10, 20);
+
+        System.out.println(suma1 + " otra suma " + suma2);
+    }
+}
